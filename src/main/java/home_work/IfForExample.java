8965fe4724443320c1e6ce1b9f69d84217a63a9e
Vach <vacho.kocharyan@mail.ru>
2022-12-20 17:10:57 +0400

    add some examples

---
+++ src/main/java/home_work/IfForExample.java
@@ -0,0 +1,38 @@
+package home_work;
+
+public class IfForExample {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c ;
+        a = 10;
+        b = 20;
+
+
+//        if (a < b) {
+//            System.out.println(" a is greater than b");
+//        } else
+//            System.err.println("wrong answer");
+//    }
+
+        for (c = 0 ; c <= b ; c++) {
+            if (c>=a){
+                System.out.println(c);
+            }
+        }
+
+        if (a == b) {
+            System.out.println("number a = b ");
+        } else {
+            System.out.println("number a != b ");
+
+        }
+        }
+
+            }
+
+
+
+
+
+
